06b85aadcf50d2f9dc85415cea77356d49d2de98
sivamani70 <sivamani.r70@gmail.com>
2024-04-21 19:57:08 +0530

    completed init script for fedora

--- .init-fedora.sh
+++ .init-fedora.sh
@@ -23,7 +23,6 @@ sudo dnf install cascadia-code-fonts -y
 # Importing MicroSoft GPG Key
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
-
 # Installing VS-Code
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf check-update
