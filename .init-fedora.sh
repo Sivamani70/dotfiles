7dd782b66288cdb9aaa41ba07ff090e81dca2f49
Siva Mani <sivamani.r70@gmail.com>
2024-12-29 23:29:12 +0530

    new modifications

--- .init-fedora.sh
+++ .init-fedora.sh
@@ -1,41 +1,52 @@
 #!/bin/bash
 
+
+# Run this in the root terminal [Removed all the sudo keywords]
+
 # System update
-sudo dnf update -y && sudo dnf upgrade -y && sudo dnf autoremove -y
+echo "Performing system update"
+dnf update -y &&  dnf upgrade -y &&  dnf autoremove -y
 
 # Setting Up Git
+echo "Setting up git values"
 git config --global user.name "sivamani70"
 git config --global user.email "sivamani.r70@gmail.com"
 git config --global init.defaultBranch "main"
 git config --global core.editor "code --wait"
 
 # VIM install
-sudo dnf install vim-enhanced -y
-# Installing Google-Chorme
-sudo dnf install google-chrome-stable -y
+echo "Installing VIM"
+ dnf install vim-enhanced -y
 
-# Insatlling Gnome-Tweaks
-sudo dnf install gnome-tweaks -y
+# Installing Google-Chrome
+echo "Installing Google Chrome"
+dnf install google-chrome-stable -y
+
+# Installing Gnome-Tweaks
+echo "Installing Gnome Tweaks"
+dnf install gnome-tweaks -y
 
 # Cascadia code font install 
-sudo dnf install cascadia-code-fonts -y
+echo "Installing Cascadia Code Fonts"
+dnf install cascadia-code-fonts -y
 
-# Importing MicroSoft GPG Key
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+# Installing gcc
+echo "Installing gcc"
+dnf install gcc -y
 
 # Installing VS-Code
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf check-update
-sudo dnf install code -y
+echo "Installing VS Code"
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" |  tee /etc/yum.repos.d/vscode.repo > /dev/null
+dnf check-update
+dnf install code -y
 
-curl -sSL -O https://packages.microsoft.com/config/fedora/39/packages-microsoft-prod.rpm
-sudo rpm -i packages-microsoft-prod.rpm
-rm packages-microsoft-prod.rpm
-sudo dnf update
+
+
 
 # Installing MS Edge
-sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
-sudo dnf install microsoft-edge-stable -y
+#  dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
+#  dnf install microsoft-edge-stable -y
 
-# Installing powershell
-sudo dnf install https://github.com/PowerShell/PowerShell/releases/download/v7.4.2/powershell-7.4.2-1.rh.x86_64.rpm -y
+
